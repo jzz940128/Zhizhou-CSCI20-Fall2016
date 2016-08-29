81451c2384ffacaf79f6108e7cdeab421f9faa0a
Zhizhou <zjiang001@student.butte.edu>
2016-08-29 18:08:25 +0000

    lab1 folder updated for ASCII

---
+++ lab1/lab1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+/* Lab1 - ASCII art
+   Author: Zhizhou Jiang
+   Class:  CSCI20 Browne
+   Date:   08/29/2016
+   Description: Use characters to Output picture.
+   */
+   
+int main() {
+    cout<<"                O     "<<endl;
+    cout<<"               o      "<<endl;
+    cout<<"              o       "<<endl;
+    cout<<"      **-----| |-*    "<<endl;
+    cout<<"     *  *         *   "<<endl;
+    cout<<"    *____* ________*  "<<endl;
+    cout<<"    | _ _ |   ___  |  "<<endl;
+    cout<<"    ||_|_||  |   | |  "<<endl;
+    cout<<"    |     |  |*  | |  "<<endl;
+    cout<<"    |____ |__|___|_|  "<<endl;
+    cout<<"                      "<<endl;
+    
+    return 0;
+      }
