5efe6d6a44dbf095c00078795542d27ad8cc47f9
Zhizhou <zjiang001@student.butte.edu>
2016-08-30 03:42:36 +0000

    Lab 2 submission

---
+++ lab2/lab2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+/* Lab1 - Guess game
+   Author: Zhizhou Jiang
+   Class:  CSCI20 Browne
+   Date:   08/29/2016
+   Description: user guess the number between two random numbers
+   */
+   int main() {
+    int user = 0;
+    int pc_num1;
+    int pc_num2;
+    pc_num1 = random%11;
+    pc_num2 = random%11;
+    cout<<"please enter the a number";
+    cin>> user;
+    if(user = nu)
