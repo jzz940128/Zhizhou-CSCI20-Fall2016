bc278fef49d0c72d661c9701e3386767697c7640
Zhizhou <zjiang001@student.butte.edu>
2016-09-13 04:58:31 +0000

    Lab 5 Submission

--- lab5/lab5.cpp
+++ lab5/lab5.cpp
@@ -46,13 +46,46 @@ using namespace std;
    cin>>monster1.Mouth;
 
 // output the whole monsters
-   cout<<"the name of Your monster is: "<<monster1.Name<<endl;
-   cout<<"the head of Your monster is: "<<monster1.Head<<endl;
-   cout<<"the eyes of Your monster is: "<<monster1.Eyes<<endl;
-   cout<<"the ears of Your monster is: "<<monster1.Ears<<endl;
-   cout<<"the nose of your monster is: "<<monster1.Nose<<endl;
-   cout<<"the mouth of Your monster is: "<<monster1.Mouth<<endl;
- 
-   
+   cout<<"-------------------------------------------------------------------------"<<endl;
+   cout<<"Here is the first monster u build: "<<monster1.Name<<endl;
+   cout<<"head: "<<monster1.Head<<", eyes: "<<monster1.Eyes<<", ears: "<<monster1.Ears;
+   cout<<"nose: "<<monster1.Nose<<", mouth: "<<monster1.Mouth<<endl;
+
+//buid the second monster
+  monster2.Name ="Joe";
+  monster2.Head ="Happy";
+  monster2.Eyes ="Wackus";
+  monster2.Ears ="Spritem";
+  monster2.Nose ="Zombus";
+  monster2.Mouth ="Spritem";
+   cout<<"-------------------------------------------------------------------------"<<endl;
+   cout<<"Here is the second monster u build: "<<monster2.Name<<endl;
+   cout<<"head: "<<monster2.Head<<", eyes: "<<monster2.Eyes<<", ears: "<<monster2.Ears;
+   cout<<"nose: "<<monster2.Nose<<", mouth: "<<monster2.Mouth<<endl;
+
+//buid the third monster
+  monster3.Name ="FengShao";
+  monster3.Head ="Franken";
+  monster3.Eyes ="Vegitas";
+  monster3.Ears ="Wackus";
+  monster3.Nose ="Spritem";
+  monster3.Mouth ="Vegitas";
+   cout<<"-------------------------------------------------------------------------"<<endl;
+   cout<<"Here is the third monster u build: "<<monster3.Name<<endl;
+   cout<<"head: "<<monster3.Head<<", eyes: "<<monster3.Eyes<<", ears: "<<monster3.Ears;
+   cout<<"nose: "<<monster3.Nose<<", mouth: "<<monster3.Mouth<<endl;
+  
+//build the fourth monster
+  monster4.Name ="AntiMagic";
+  monster4.Head ="long";
+  monster4.Eyes ="none";
+  monster4.Ears ="elf";
+  monster4.Nose ="spritem";
+  monster4.Mouth ="Wackus";
+  cout<<"-------------------------------------------------------------------------"<<endl;
+   cout<<"Here is the fourth monster u build: "<<monster4.Name<<endl;
+   cout<<"head: "<<monster4.Head<<", eyes: "<<monster4.Eyes<<", ears: "<<monster4.Ears;
+   cout<<"nose: "<<monster4.Nose<<", mouth: "<<monster4.Mouth<<endl;
+
    return 0;
  }
