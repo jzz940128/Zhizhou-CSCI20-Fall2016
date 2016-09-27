b87065327574ba47499c13bba87659f6b106374f
Zhizhou <zjiang001@student.butte.edu>
2016-09-27 07:16:19 +0000

    Lab8 resubmission

--- lab8/lab8.cpp
+++ lab8/lab8.cpp
@@ -6,31 +6,26 @@ using namespace std;
    Date:   09/25/2016
    Description: create a program to convert Kilogram and Pounds
                 use 2 functions and return their converting value.
-                In my program I use the equation: kilograms = pounds / 2.
-                and pounds = kilograms * 2.
+                In my program I use the equation: kilograms = pounds / 2.2
+                and pounds = kilograms * 2.2
  */
 
 // declare the first functions Kilogram to Pounds
-float ToPounds() {
- float kg =0.0;// the varibale of kilograms taht the user can input a value in below
+float ToPounds(float kg) {
+ // the varibale of kilograms taht the user can input a value in below
  cin>>kg;
- cout<<"You input: "<<kg<<" kg."<<endl;
- cout<<"translating to pounds..."<<endl;
- float pounds = 0.0; // the variable of pounds 
+  
+ float pounds = 0.0; // the variable of pounds to store the value of the pounds
  pounds = kg * 2.2; // computing the value of pounds
- cout<<"The equality value in pounds is : "<<pounds<<" lbs"<<endl;
  return pounds;
  
 }
 // declare the second function Pounds to Kilogram
-float ToKilo() {
-    float pounds =0.0; // the variable of Pounds that user can input value in below
-    float kg =0.0; // the variable of kilogram
+float ToKilo(float pounds) {
+     // the variable of Pounds that user can input value in below
+    float kg =0.0; // the variable of kilogram to store the value of kilogram.
     cin>> pounds;
-    cout<<"You input: "<<pounds<<" lbs."<<endl;
-    cout<<"translating to kilograms..."<<endl;
     kg = pounds/2.2; // computing the value of kilogram
-    cout<<"The equality value in kilograms is : "<<kg<<" kg"<<endl;
     return kg;
 }
 
@@ -38,13 +33,13 @@ float ToKilo() {
 int main() {
 cout<<"This is the program to convert Kilograms and pounds"<<endl;
 cout<<"The first convert is Kilograms to Pounds, and Pounds to Kilograms after."<<endl;
-cout<<"Kilograms to pounds start: "<<endl;
+cout<<"Please enter any number of Kilogram: "<<endl;
 
 // call the function converting kilogram to pounds
- ToPounds();
-cout<<"Pounds to kilograms start: "<<endl;
+ cout<<ToPounds(0.0)<<endl;;
+cout<<"Please enter any number of Pounds: "<<endl;
 // call the function converting pounds to kilogram
- ToKilo();
+ cout<<ToKilo(0.0)<<endl;
  
  return 0;
 }
